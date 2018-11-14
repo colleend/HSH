7fc83f7977fd072571c629080f3116e29ee173e5
colleend <colleend21@gmail.com>
2018-11-14 15:09:28 -0800

    Intersections includes all nodenums, works

--- firstImp.py
+++ firstImp.py
@@ -1,29 +1,19 @@
-import xml.etree.ElementTree
+import lxml.etree
+import collections
 
 pathToXML = "/Users/colleendai/Desktop/School/Computer Science/CS221/project/map.xml"
 
-treeRoot = xml.etree.ElementTree.parse(pathToXML).getroot()
+treeRoot = lxml.etree.parse(pathToXML).getroot()
 
-nodes = {}
-for anode in treeRoot.findall("node"):
-	#print (anode.tag, anode.attrib)
-	nodeNum = anode.get('id')
-	lat = anode.get('lat')
-	lon = anode.get('lon')
-	if (not nodeNum in nodes):
-		nodes[nodeNum] = [1, lat, lon]
-	else:
-		print ("in els")
-		nodes[nodeNum][0] += 1
-		break
+nodes = collections.defaultdict(int)
+for anode in treeRoot.findall("./way/nd"):
+	nodeNum = anode.get('ref')
+	nodes[nodeNum] += 1
 
 intersections = []
-for nodeNum, info in nodes.iteritems():
-	#print (info)
-	count, lat, lon = info
+for nodeNum, count in nodes.iteritems():
 	if (count > 1):
-		intersections.append((lat, lon))
-		print ("intersect found! " + str(info))
+		intersections.append((nodeNum))
 
 print (intersections)
 
