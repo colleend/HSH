92f58a66040e35a5e04115b57141ba83c2834967
colleend <colleend21@gmail.com>
2018-11-15 14:43:43 -0800

    got all the intersection nodenum, lat, lon

--- firstImp.py
+++ firstImp.py
@@ -1,34 +1,46 @@
 import lxml.etree
+import csv
 import collections
 
 pathToXML = "/Users/colleendai/Desktop/School/Computer Science/CS221/project/map.xml"
+newFileName = "/Users/colleendai/Desktop/School/Computer Science/CS221/project/nodeLocs.csv"
+newNodeFileName = "/Users/colleendai/Desktop/School/Computer Science/CS221/project/nodes.csv"
 
-treeRoot = lxml.etree.parse(pathToXML).getroot()
+doc = lxml.etree.parse(pathToXML)
+treeRoot = doc.getroot()
 
+# Go through all the nodes, make a dictionary of nodeNum -> #occurences
 nodes = collections.defaultdict(int)
 for anode in treeRoot.findall(".//way//nd"):
 	nodeNum = anode.get('ref')
 	nodes[nodeNum] += 1
 
+# Get all the intersections (count > 1)
 intersections = []
 for nodeNum, count in nodes.iteritems():
 	if (count > 1):
 		intersections.append(nodeNum)
 
-locNodes = []
-print (intersections)
-'''for nodeNum in intersections:
-	searchstring = ".//node/..[@id='%s']" %nodeNum
-	node = treeRoot.find(searchstring)
-	lat = node.get('lat')
-	lon = node.get('lon')
-	num = node.get('id')
-	locNodes.append([num, lat, lon])'''
-'''for node in treeRoot.findall(".//node"):
-	num = int(node.get('id'))
-	if (num in intersections):
-		lat = node.get('lat')
-		lon = node.get('lon')
-		locNodes.append([num, lat, lon])'''
+# Write to csv file
+with open(newNodeFileName, 'wb') as f:
+	nodeWriter = csv.writer(f, delimiter=' ')
+	for node in intersections:
+		nodeWriter.writerow([node])
+
+# Get a dict of nodeNum : lat, lon
+nodeInfo = collections.defaultdict(int)
+for anode in treeRoot.findall("node"):	
+	nodeNum = anode.get('id')
+	lat = anode.get('lat')
+	lon = anode.get('lon')
+	nodeInfo[nodeNum] = (lon, lat) #Lon x, Lat y
+
+# Write to csv file of intersection node num, lat, lon
+with open(newFileName, 'wb') as csvfile:
+	locWriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+	for nodeNum in intersections:
+		lon, lat = nodeInfo[nodeNum]
+		locWriter.writerow([nodeNum, lat, lon])
+
 
 
